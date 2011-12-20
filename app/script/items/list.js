7ed01f1bd2b5b0ada4601a49997e22fa6c0e85e8
eric-wang <starwz@gmail.com>
2011-12-20 13:36:38 +0800

    create in-momery data model for list view

--- app/script/items/list.js
+++ app/script/items/list.js
@@ -1,6 +1,8 @@
 define(["dojo/dom", "dojo/_base/connect", "dijit/registry", "dojox/mvc","dojo/data/ItemFileWriteStore", "dojo/store/DataStore", "dojox/mobile/TransitionEvent"], 
 	function(dom, connect, registry, mvc, itemfilewritestore, datastore, TransitionEvent){
 	return {
+		dataFile: ["items-for-0.json", "items-for-1.json", "items-for-2.json"],
+
 		init: function(){
 			// override ListItem onClick event
 			function showItemDetails(node, index, e){
@@ -17,42 +19,28 @@ define(["dojo/dom", "dojo/_base/connect", "dijit/registry", "dojox/mvc","dojo/da
 			};
 			window.showItemDetails = showItemDetails;
 
+			// show data
 			if(!window.selected_configuration_item){
 				window.selected_configuration_item = 0;
 			}
+			app.loadedModels.itemlistsmodel = []; // initial the cache models
+			this.showData();
 
-			var select_data = app.loadedModels.listsmodel[window.selected_configuration_item];
-			var writeStroe = new itemfilewritestore({url: select_data.itemsurl.data});
-			app.loadedModels.itemlistsmodel = []; // cache the loaded model in array
-
-			var modelPromise = mvc.newStatefulModel({store: new datastore({store: writeStroe})});
-			modelPromise.then(dojo.hitch(this, function(datamodel){
-				var listId = datamodel[0].parentId.data;
-				if(listId == window.selected_configuration_item){
-					this.loadedModels.itemlistmodel = datamodel;
-					app.loadedModels.itemlistmodel = datamodel;
-					app.loadedModels.itemlistsmodel[listId] = datamodel;
-
-					var widget = registry.byId("itemlist_repeat");
-					widget.ref = null;
-					widget.set("ref", datamodel);
-				}
-			}));
-
-			connect.connect(dom.byId('items_list_edit'), 'click', dojo.hitch(this, function(e){
-				var datamodel = this.loadedModels.itemlistmodel;
-				var node = e.srcElement;
+			// connect edit button
+			connect.connect(dom.byId('items_list_edit'), "click", function(){
 				var transOpts = {
 					title:"List Edit",
 					target:"items,list_edit",
 					url: "#items,list_edit"
 				};
-				new TransitionEvent(node,transOpts,e).dispatch();
-			}));
+				var e = window.event;
+				new TransitionEvent(e.srcElement,transOpts,e).dispatch();
+			});
 		},
 
-		activate: function(){
+		showData: function(){
 			var select_data = app.loadedModels.listsmodel[window.selected_configuration_item];
+			// get data model in cache
 			if(app.loadedModels.itemlistsmodel[select_data.id.data]){ // read data from cache
 				app.loadedModels.itemlistmodel = app.loadedModels.itemlistsmodel[select_data.id.data];
 
@@ -61,21 +49,50 @@ define(["dojo/dom", "dojo/_base/connect", "dijit/registry", "dojox/mvc","dojo/da
 				widget.set("ref", app.loadedModels.itemlistmodel);
 				return;
 			}
+			// create in-memory store if the file not exists
+			// TODO: use the exists file in an array in this demo.
+			else if(!isFileExist(select_data.itemsurl.data, this.dataFile)){
+				console.log("file not exist.", select_data.itemsurl.data);
+				var tempStore = new dojo.store.Memory({ "data" : []});
+				var datamodel = mvc.newStatefulModel({
+					"store": tempStore
+				});
+				app.loadedModels.itemlistmodel = datamodel;
+				app.loadedModels.itemlistsmodel[select_data.id.data] = datamodel;
 
-			// not in cache, load data model from data file
-			var writeStroe = new itemfilewritestore({url: select_data.itemsurl.data});
-			var modelPromise = mvc.newStatefulModel({store: new datastore({store: writeStroe})});
-			modelPromise.then(dojo.hitch(this, function(datamodel){
-				var listId = datamodel[0].parentId.data;
-				if(listId == window.selected_configuration_item){
-					app.loadedModels.itemlistmodel = datamodel;
-					app.loadedModels.itemlistsmodel[listId] = datamodel;
+				var widget = registry.byId("itemlist_repeat");
+				widget.ref = null;
+				widget.set("ref", datamodel);
+			}
+			// load data model from data file
+			else{
+				var writeStroe = new itemfilewritestore({url: select_data.itemsurl.data});
+				var modelPromise = mvc.newStatefulModel({store: new datastore({store: writeStroe})});
+				modelPromise.then(dojo.hitch(this, function(datamodel){
+					var listId = datamodel[0].parentId.data;
+					if(listId == window.selected_configuration_item){
+						app.loadedModels.itemlistmodel = datamodel;
+						app.loadedModels.itemlistsmodel[listId] = datamodel;
 
-					var widget = registry.byId("itemlist_repeat");
-					widget.ref = null;
-					widget.set("ref", datamodel);
+						var widget = registry.byId("itemlist_repeat");
+						widget.ref = null;
+						widget.set("ref", datamodel);
+					}
+				}));
+			};
+
+			function isFileExist(filename, files){
+				for(var file in files){
+					if(filename.indexOf(files[file]) > 0){
+						return true;
+					}
 				}
-			}));
+				return false;
+			};
+		},
+
+		activate: function(){
+			this.showData();
 		},
 
 		deactivate: function(){
