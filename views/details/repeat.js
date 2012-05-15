19e6706bdf5277d41c86be97b00cc4acc4b6c606
Christophe Jolif <cjolif@gmail.com>
2012-05-15 18:16:37 +0200

    workaround jQuery mobile issue #4364

--- views/details/repeat.js
+++ views/details/repeat.js
@@ -1,14 +1,12 @@
 define.amd.jQuery = true;
-define(["jquery", "jquerym"],
-	function($){
-
-	// need to disable jQuery Mobile hash support that it clashes with dojox/app own support
-	$.mobile.hashListeningEnabled = false;
-	// need to disable jQuery Mobile pushState support
-	$.mobile.pushStateEnabled = false;
-
-		// $.mobile.autoInitializePage = false
-		// data-role="none"
+define(["jquery"], function($){
+	// we need to do it like that to workaround jQuery mobile #4364
+	$.mobile = {
+		// need to disable jQuery Mobile hash support that it clashes with dojox/app own support
+		hashListeningEnabled: false,
+		// need to disable jQuery Mobile pushState support
+		pushStateEnabled: false
+	};
 	var itemlistmodel = null;
 
 	var refreshData = function(){
@@ -33,26 +31,28 @@ define(["jquery", "jquerym"],
 		init: function(){
 			this.loadedModels.itemlistmodel = todoApp.currentItemListModel;
 			itemlistmodel = this.loadedModels.itemlistmodel;
-
-			// connect a listener on the list that does update the model
-			$("#list_repeat").change(function(e){
-			    // stuff
-				var index = parseInt(e.target.value)-1;
-				var datamodel = itemlistmodel.model[todoApp.selected_item];
-				if(datamodel){
-					datamodel.repeat = index;
-				}
+			// need to load mobile on init, needs to do it here to allow room for
+			// the hack to workaround #4364
+			require(["jquerym"], function(){
+				// connect a listener on the list that does update the model
+				$("#list_repeat").change(function(e){
+					var index = parseInt(e.target.value)-1;
+					var datamodel = itemlistmodel.model[todoApp.selected_item];
+					if(datamodel){
+						datamodel.repeat = index;
+					}
+				});
 			});
 		},
 
 		beforeActivate: function(){
 			this.loadedModels.itemlistmodel = todoApp.currentItemListModel;
 			itemlistmodel = this.loadedModels.itemlistmodel;
-			refreshData();
+			refreshData($);
 		},
 
 		destroy: function(){
 			// disconnect listener on the list
 		}
-	}
+	};
 });
