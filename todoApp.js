5cdd6671eabc736e2fe67617f2bf64d64ba5c3ac
Christophe Jolif <cjolif@gmail.com>
2012-07-11 16:46:34 +0200

    simplify code

--- todoApp.js
+++ todoApp.js
@@ -1,13 +1,13 @@
-define(["dojo/_base/declare", "dojox/mobile/ProgressIndicator"], function(declare, ProgressIndicator){
-	return declare(null, {
+define(["dojox/mobile/ProgressIndicator"], function(ProgressIndicator){
+	return function(){
 		// the default select_item is 0, or will throw an error if directly transition to #details,EditTodoItem view
-		selected_item: 0,
-		selected_configuration_item: 0,
-		progressIndicator: null,
+		this.selected_item = 0;
+		this.selected_configuration_item = 0;
+		this.progressIndicator = null;
 		/*
 	 	* show or hide global progress indicator
 	 	*/
-		showProgressIndicator: function(show){
+		this.showProgressIndicator = function(show){
 			if(!this.progressIndicator){
 				this.progressIndicator = ProgressIndicator.getInstance({removeOnStop:false, startSpinning:true, size:40, center:true, interval:30});
 				// TODO: use dojo.body will throw no appendChild method error.
@@ -23,5 +23,5 @@ define(["dojo/_base/declare", "dojox/mobile/ProgressIndicator"], function(declar
 				this.progressIndicator.domNode.style.visibility = "hidden";
 			}
 		}
-	});
+	};
 });
