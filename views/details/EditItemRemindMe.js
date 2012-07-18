0b0ae9c46ef13b5f30227eee57bc892da1ce0cd3
Ed Chatelain <ed.chatelain@gmail.com>
2012-07-18 16:08:00 -0400

    Revert EditItemRemindMe.js back to use dom events because of a problem on ipad where the opener was getting _blur notified and closing the date picker right away.

--- views/details/EditItemRemindMe.js
+++ views/details/EditItemRemindMe.js
@@ -1,7 +1,8 @@
-define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojox/mvc/at",
+define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dojo/on", "dijit/registry", "dojox/mvc/at",
         "dojo/date/stamp", "dojo/dom-class"],
-	function(dom, lang, domStyle, registry, at, stamp, domClass){
+	function(dom, lang, domStyle, on, registry, at, stamp, domClass){
 	var itemlistmodel = null;
+	var signals = [];
 
 	var showDateDialog = function(widgetid){
 		var datamodel = itemlistmodel.model[this.app.selected_item];
@@ -11,12 +12,12 @@ define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojo
 		}
 		//console.log("remind showDateDialog date = ", date);
 		registry.byId("reminddlgpicker1").set("value", date);							
-		registry.byId('datePicker').show(dom.byId(widgetid),['above-centered','below-centered','after','before']);
+		registry.byId("datePicker").show(dom.byId(widgetid),['above-centered','below-centered','after','before']);
 	};	
 	
 	var refreshData = function(){
 		var datamodel = itemlistmodel.model[this.app.selected_item];
-		if(datamodel){
+		if (datamodel) {
 			// need to add reminderOnAday property to the original data store
 			var widget = registry.byId('remind_day_switch');
 			widget.set("value", at(datamodel, "reminderOnAday"));
@@ -27,7 +28,7 @@ define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojo
 
 			//set remind time
 			widget = registry.byId('remind_date');
-			if(widget){
+			if (widget) { 
 				widget.set("label", at(datamodel, "reminderDate"));	
 				var value = datamodel.get("reminderDate");
 				if(value && value < stamp.toISOString(new Date(), {selector: "date"})){
@@ -46,8 +47,9 @@ define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojo
 	return {
 		init: function(){
 			this.loadedModels.itemlistmodel = this.app.currentItemListModel;
-			itemlistmodel = this.loadedModels.itemlistmodel;
-
+			itemlistmodel = this.loadedModels.itemlistmodel
+			var signal;
+		
 			registry.byId("remind_day_switch").on("stateChanged", lang.hitch(this, function(newState){
 				//console.log("remind_day_switch = ", newState);
 				// update remind on a day value to the data model
@@ -73,12 +75,13 @@ define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojo
 				datamodel.set("reminderOnAlocation",newState);				
 			}));
 
-			registry.byId("remind_date").on("click", lang.hitch(this, function(){
+			signal = on(dom.byId("remind_date"), "click", lang.hitch(this, function(){
 				//console.log("remind_date clicked call showDateDialog ");
 				showDateDialog('remind_date');
 			}));
+			signals.push(signal);
 
-			registry.byId("reminddlgSet").on("click", lang.hitch(this, function(){
+			signal = on(dom.byId("reminddlgSet"), "click", lang.hitch(this, function(){
 				//console.log("reminddlgSet clicked ");
 				// update remind on a day value to the data model
 				var datamodel = this.loadedModels.itemlistmodel.model[this.app.selected_item];
@@ -92,21 +95,23 @@ define(["dojo/dom", "dojo/_base/lang", "dojo/dom-style", "dijit/registry", "dojo
 				}else{
 					datamodel.set("reminderDate", date);
 					domClass.remove(registry.byId('remind_date').domNode, "dateLabelInvalid");
-					registry.byId("datePicker").hide(true);
+					registry.byId('datePicker').hide(true);
 					domStyle.set(dom.byId("invalidDate"), "visibility", "hidden");
 				}
 			}));
+			signals.push(signal);
 
-			registry.byId("reminddlgCancel").on("click", lang.hitch(this, function(){
+			signal = on(dom.byId("reminddlgCancel"), "click", lang.hitch(this, function(){
 				//console.log("reminddlgCancel clicked ");
 				domStyle.set(dom.byId("invalidDate"), "visibility", "hidden");
-				registry.byId("datePicker").hide(false);
+				registry.byId("datePicker").hide(false)
 				var datamodel = itemlistmodel.model[this.app.selected_item];
 				date = datamodel.get("reminderDate");
 				if(!date){ // cancelled and no date set, so need to set 
 					datamodel.set("reminderOnAday", "off");
 				}
 			}));
+			signals.push(signal);
 		},
 
 		beforeActivate: function(){
