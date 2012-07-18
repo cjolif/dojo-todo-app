9c88205afefaadf34c2e11938a1306b15bd044be
Christophe Jolif <cjolif@gmail.com>
2012-07-18 15:27:35 +0200

    come back on previous  commit as this was not the origin of the encountered issue

--- todoApp.js
+++ todoApp.js
@@ -1,27 +1,27 @@
-define(["dojo/_base/declare", "dojox/mobile/ProgressIndicator"], function (declare, ProgressIndicator){
-	return declare(null, {
+define(["dojox/mobile/ProgressIndicator"], function(ProgressIndicator){
+	return function(){
 		// the default select_item is 0, or will throw an error if directly transition to #details,EditTodoItem view
-		selected_item:0,
-		selected_configuration_item:0,
-		progressIndicator:null,
+		this.selected_item = 0;
+		this.selected_configuration_item = 0;
+		this.progressIndicator = null;
 		/*
-		 * show or hide global progress indicator
-		 */
-		showProgressIndicator:function (show){
+	 	* show or hide global progress indicator
+	 	*/
+		this.showProgressIndicator = function(show){
 			if(!this.progressIndicator){
-				this.progressIndicator=ProgressIndicator.getInstance({removeOnStop:false, startSpinning:true, size:40, center:true, interval:30});
+				this.progressIndicator = ProgressIndicator.getInstance({removeOnStop:false, startSpinning:true, size:40, center:true, interval:30});
 				// TODO: use dojo.body will throw no appendChild method error.
-				var body=document.getElementsByTagName("body")[0];
+				var body = document.getElementsByTagName("body")[0];
 				body.appendChild(this.progressIndicator.domNode);
-				this.progressIndicator.domNode.style.zIndex=999;
+				this.progressIndicator.domNode.style.zIndex = 999;
 			}
 			if(show){
-				this.progressIndicator.domNode.style.visibility="visible";
+				this.progressIndicator.domNode.style.visibility = "visible";
 				this.progressIndicator.start();
-			} else{
+			}else{
 				this.progressIndicator.stop();
-				this.progressIndicator.domNode.style.visibility="hidden";
+				this.progressIndicator.domNode.style.visibility = "hidden";
 			}
 		}
-	});
+	};
 });
