c65551ac8e3410212bd12ff11803ce96cf15cee2
Ed Chatelain <ed.chatelain@gmail.com>
2013-02-22 16:25:50 -0500

    Update the todoApp to use the has based config support.

--- src.js
+++ src.js
@@ -1,17 +1,16 @@
-require(["dojox/app/main", "dojox/json/ref"],
-	function(Application, json){
-
+require(["dojox/app/main", "dojox/json/ref", "dojo/sniff"],
+	function(Application, json, has){
 	var isTablet = false;
-
-	var configurationFile = "./config-phone.json";
+	var configurationFile = "./config.json";
 	if(window.innerWidth > 600){
-		configurationFile = "./config-tablet.json";
 		isTablet = true;
 	}
-
 	require(["dojo/text!"+configurationFile], function(configJson){
+		has.add("tablet", isTablet);
+		has.add("phone", !isTablet);
+		has.add("ie9orLess", has("ie") && !has("ie") >= 10);
+		has.add("notie9orLess", !has("ie") || has("ie") >= 10);
 		var config = json.fromJson(configJson);
-		config.isTablet = isTablet;
 		Application(config);
 	});
 });
