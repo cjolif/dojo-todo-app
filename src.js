98dabdafa46ceb4163952baca72fb41f270f8a33
Ed Chatelain <ed.chatelain@gmail.com>
2013-03-01 15:26:04 -0500

    More updates to the testcases after the has config changes.

--- src.js
+++ src.js
@@ -2,14 +2,13 @@ require(["dojox/app/main", "dojox/json/ref", "dojo/sniff"],
 	function(Application, json, has){
 	var isTablet = false;
 	var configurationFile = "./config.json";
-	if(window.innerWidth > 600){
+	var width = window.innerWidth || document.documentElement.clientWidth;
+	if(width > 600){
 		isTablet = true;
 	}
 	require(["dojo/text!"+configurationFile], function(configJson){
-		has.add("tablet", isTablet);
 		has.add("phone", !isTablet);
-		has.add("ie9orLess", has("ie") && !has("ie") >= 10);
-		has.add("notie9orLess", !has("ie") || has("ie") >= 10);
+		has.add("ie9orLess", has("ie") && (has("ie") <= 9));
 		var config = json.fromJson(configJson);
 		Application(config);
 	});
